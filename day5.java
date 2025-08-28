ff648789af9a9ac949ca0e37afbfab692b813dff
kaushal kishor gupta <kaushalkishor2724@gmail.com>
2025-08-28 19:54:03 +0530

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class day5 {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        if(n==1){
+            System.out.println(1);
+        } else if (n==2 || n==3) {
+            System.out.println("NO SOLUTION");
+        }
+        else{
+            StringBuilder sb=new StringBuilder();
+             for(int i=2;i<=n;i+=2){
+                sb.append(i).append(" ");
+             }
+             for(int i=1;i<=n;i+=2){
+                 sb.append(i).append(" ");
+
+             }
+            System.out.println(sb.toString().trim());
+        }
+
+    }
+}
