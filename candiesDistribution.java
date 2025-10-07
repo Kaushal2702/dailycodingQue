a9559b02533d9bff82d3fbf09bfc21e0b60157d9
kaushal kishor gupta <kaushalkishor2724@gmail.com>
2025-10-07 09:55:04 +0530

    Add files via upload

---
+++ candiesDistribution.java
@@ -0,0 +1,14 @@
+//import java.util.Scanner;
+//
+//public class candiesDistribution {
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        int t=sc.nextInt();
+//        while(t-->0){
+//            int n=sc.nextInt();
+//            int rem=n%3;
+//            int add=(3-rem)%3;
+//            System.out.println(add);
+//        }
+//    }
+//}
