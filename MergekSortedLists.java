573ae227d8e02c61b1ee21177d773979ada30229
kaushal kishor gupta <kaushalkishor2724@gmail.com>
2026-06-12 23:48:31 +0530

    Leetcode 23 Implement mergeKLists to merge k sorted linked lists

---
+++ MergekSortedLists.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+       Queue<ListNode> pq=new PriorityQueue<>((a,b)->Integer.compare(a.val,b.val)); 
+       for(ListNode node:lists){
+           if(node!=null) pq.add(node);
+       }
+       ListNode dummy=new ListNode();
+       ListNode result=dummy;
+       while(!pq.isEmpty()){
+          ListNode curr=pq.poll();
+          result.next=curr;
+          result=result.next;
+          if(curr.next!=null){
+            pq.add(curr.next);
+          }
+       }
+       return dummy.next;
+    }
+}
